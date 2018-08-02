f8363c93193051d65a53fb7f25370d6293fdbf55
Denis Boyda <boyda_d@mail.ru>
2018-08-02 19:11:49 +1000

    Task solved

---
+++ include/allocator.h
@@ -0,0 +1,45 @@
+#include <stdexcept>
+
+
+template <typename T, const std::size_t totalSize=1>
+class reserveAllocator{
+public:
+    
+  using value_type = T;
+  
+  reserveAllocator(): offset(0){
+    if(totalSize <=0)
+      throw std::logic_error("ReserveAllocator: totalSize must be more 0");            
+    memPtr = reinterpret_cast<T*> (::operator new (totalSize * sizeof(T)));      
+  };
+  
+  ~reserveAllocator(){
+    ::operator delete(memPtr);
+  };
+  
+  T *allocate(std::size_t n) {
+    T* ptr = nullptr;      
+    const std::size_t newOffset = offset + n;
+    if(newOffset <= totalSize){
+      ptr = memPtr + offset;
+      offset = newOffset;        
+    } else
+      throw std::bad_alloc();    
+    return ptr; 
+  }
+  
+  void deallocate(T *p, std::size_t n) {    
+    if(memPtr+offset == p+n){
+      offset -= n;
+    }
+  }
+  
+  template<typename U>
+  struct rebind {
+      using other = reserveAllocator<U, totalSize>;
+  };
+  
+private:
+  std::size_t offset;
+  T* memPtr;  
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <cmath>
 #include "version.h"
+#include "allocator.h"
+#include "container.h"
+
+template <class T>
+void fillMap(T& m){
+	for(int i=0; i< 10; ++i)
+			m[i] = std::tgamma(i+1);
+}
+
+template <class Cont, class Lambda>
+void fillContainer(Cont& c, Lambda lamb){
+  for(int i=0; i< 10; ++i)
+    lamb(c, i);
+}
+
+template<typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::map<Args...> &m){
+	for(const auto& p: m)
+			os<<p.first<<' '<<p.second<<'\n';
+	return os;
+}
 
 int main(int argc, char** argv)
 {
-  std::cout<<"Hello, World!\n";
-  std::cout<<"\t"<<gGIT_VERSION<<"\n";
-  std::cout<<std::endl;
+  auto setMap = [](auto& cont, const auto& i) {cont[i] = std::tgamma(i+1);};
+    
+//  std::cout<<"\nDefault Allocator\n";
+  std::map<int, int> md;
+  fillContainer(md, setMap);
+//  std::cout<<md;		
+
+  std::cout<<"\nReserve Allocator\n";
+  std::map<int, int, std::less<int>, reserveAllocator<int,10> > mr;
+  fillContainer(mr, setMap);
+  std::cout<<mr;
+		
+  
+  auto setCont = [](auto& container, const auto& i) {container.pushBack(i);};
+              
+//  std::cout<<"\nContainer: default allocator\n";
+  Container<int> con1;            
+  fillContainer(con1, setCont);    
+//  std::cout<<con1;
+
+  std::cout<<"\nContainer: custom allocator\n";
+  Container<int, reserveAllocator<int,10> > con2;
+  fillContainer(con2, setCont);    
+  std::cout<<con2;
+//  
 	return 0;
 }
